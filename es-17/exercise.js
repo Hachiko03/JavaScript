83bc358167fe8ad3bdef7586168a1984bf72ae88
Jeniffer Flores <hachikofu03@gmail.com>
2023-07-22 16:23:40 +0200

    ejercicio 17

---
+++ es-17/exercise.js
@@ -0,0 +1,13 @@
+let age = 12;
+
+let question = age >= 18;
+
+let answer = " ";
+if (question) {
+    answer = "You can drive a car"
+    console.log(answer)
+}
+else {
+    answer = "You are too young to drive"
+    console.log(answer)
+}
